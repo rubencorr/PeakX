1e6e18c17bc3809d7ff717510461ccfb03afb8e2
ruben_corr <rubencorr@users.noreply.github.com>
2024-02-20 17:42:06 +0100

    Initial Commit

---
+++ PeakX/PeakXApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeakXApp.swift
+//  PeakX
+//
+//  Created by Rúben Correia on 20.02.24.
+//
+
+import SwiftUI
+
+@main
+struct PeakXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
